50ceb38ed3f7b22401881bfe0fc2a0b0ea5e76d4
Felipe Saraiva <felipe.costa@afya.com.br>
2022-02-26 15:36:22 -0300

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Felipe Costa on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
